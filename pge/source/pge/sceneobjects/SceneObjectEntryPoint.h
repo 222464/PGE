316c79e0711fa366694855ffdd2c81051aaa24ae
Eric Laukien <ericlaukien@gmail.com>
2021-01-25 19:07:51 -0500

    Updated

---
+++ pge/source/pge/sceneobjects/SceneObjectEntryPoint.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "../scene/Scene.h"
+
+class SceneObjectEntryPoint: public pge::SceneObject {
+public:
+	// Inherited from SceneObject
+	void onAdd();
+
+	SceneObject* copyFactory() {
+		return new SceneObjectEntryPoint(*this);
+	}
+};
